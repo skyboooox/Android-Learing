9d896cd026be251d240f3487df44d8e0eb6f031f
zcj924 <zcj924@gmail.com>
2020-05-28 13:46:33 +0800

    支持自定义大小数

--- AndroidStudioProjects/random/app/src/main/java/com/zcj924/random/MainActivity.java
+++ AndroidStudioProjects/random/app/src/main/java/com/zcj924/random/MainActivity.java
@@ -2,7 +2,9 @@ package com.zcj924.random;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.TextView;
 
@@ -16,12 +18,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void BtnGen_OnClicked(View v){
-        TextView textResult = (TextView)findViewById(R.id.textResult);
-        int min=10;
-        int max=99;
-        Random random = new Random();
-        int num = random.nextInt(max)%(max-min+1) + min;
 
-        textResult.setText(String.valueOf(num));
+        TextView textResult = (TextView)findViewById(R.id.textResult);
+        TextView TxtNumMax = (TextView)findViewById(R.id.TxtNumMax);
+        TextView TxtNumMin = (TextView)findViewById(R.id.TxtNumMin);
+       if (TextUtils.isEmpty(TxtNumMax.getText())||TextUtils.isEmpty(TxtNumMin.getText())){
+            if (TextUtils.isEmpty(TxtNumMax.getText())) {
+                TxtNumMax.setBackgroundColor(Color.RED);//判断为空设置红色背景
+                TxtNumMax.setHint("请输入:随机最大数字");
+            }else if (TextUtils.isEmpty(TxtNumMax.getText())) {
+                TxtNumMax.setBackgroundColor(Color.TRANSPARENT);//取消设置背景色
+            }
+            if (TextUtils.isEmpty(TxtNumMin.getText())) {
+                TxtNumMin.setBackgroundColor(Color.RED);
+                TxtNumMin.setHint("请输入:随机最小数字");
+            }else if (TextUtils.isEmpty(TxtNumMin.getText())) {
+                TxtNumMin.setBackgroundColor(Color.TRANSPARENT);
+            }
+        }else {
+           int min = Integer.parseInt(TxtNumMin.getText().toString());//取值,注意转换类型
+           int max = Integer.parseInt(TxtNumMax.getText().toString());
+           if (min>max){
+               TxtNumMax.setBackgroundColor(Color.RED);//判断为空设置红色背景
+               textResult.setText("会不会数数?大小分不清?");
+           }else {
+               Random random = new Random();
+               int num = random.nextInt(max) % (max - min + 1) + min;
+               textResult.setText(String.valueOf(num));//输出,注意转换类型
+           }
+       }
     }
 }
